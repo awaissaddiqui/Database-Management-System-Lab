a847d06dc80d4a644aeecf2779b446e9a0116d7d
Awais_Saddiqui <awaissaddiqui143@gmail.com>
2024-05-06 16:39:37 +0500

    Adding style to border in views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,18 +4,41 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    <style>
+        /* CSS for hover effect */
+        .hoverable-table tbody tr:hover {
+            background-color: rgb(51, 51, 51); 
+            color: #ffffff; 
+        }
+        thead {
+            height: 50px;
+            font-weight: bolder
+            font-size: 40px;
+        }
+        table{
+            border-collapse: collapse;
+            width: 100%;
+
+        }
+        table th , td{
+            border: 1px solid #0c0c0c;
+            padding: 8px;
+            text-align: left;
+        }
+       
+    </style>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 hoverable-table overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
                     
                     <div class="grid grid-cols-3 gap-4">
                         
                         <div class="p-6 bg-white">
-                            <h3 class="text-lg font-semibold mb-4">{{ __('Employee Data') }}</h3>
+                           
                             <div class="overflow-x-auto">
-                                <table class="min-w-full border-separate divide-y divide-gray-200 border-spacing-* border border-slate-700 dark:divide-gray-700">
+                                <table class="min-w-full border-separate divide-y  divide-gray-200 border-spacing-* border border-slate-700 dark:divide-gray-700">
                                     <thead class="bg-gray-50 dark:bg-gray-800">
                                         <tr>
                                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">{{ __('ID') }}</th>
